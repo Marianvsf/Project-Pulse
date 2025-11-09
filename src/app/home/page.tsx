dab27d444e07020f31cc388244533c5c2901064b
Marianvsf <marianvsf@gmail.com>
2025-11-08 22:15:11 -0400

    home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -97,27 +97,18 @@ export default function HomePage() {
                 </div>
             </header>*/}
             <Header />
-            <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-1 pb-10 sm:p-20">
-                <main className="flex flex-col md:flex-row items-center justify-center row-start-2 w-full">
-                    <div className="flex flex-col items-start m-5 mr-1">
-                        <h1 className="m-5 font-poppins text-[74px] leading-[117%] font-normal">¡Bienvenido a Project Pulse!</h1>
-                        <h6 className="rounded-xl font-poppins text-[20px] leading-[175%] max-w-md p-5 mt-4 tracking-wide">
-                            Sigue el pulso de tu proyecto, siente el progreso.</h6>
-                        <button onClick={goToLogin}
-                            className="h-[54px] w-[247px] text-xl rounded-full mt-4 ml-[85px] text-white md:hover:text-orange-600 transition-colors duration-200 flex justify-center items-center shadow-md bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-800">
-                            Iniciar Sesión</button>
-                    </div>
-                    <div className="mt-2 md:mt-0">
-                        <Image
-                            className="w-[488px] h-[375px] mb-5"
-                            src="/IconLogo.png"
-                            alt="symbol"
-                            width={488}
-                            height={375}
-                        />
-                    </div>
-                </main >
-            </div >
+            <main className="flex flex-col items-center text-center justify-center m-auto w-full">
+                <button onClick={goToLogin}
+                    className="h-[54px] w-[247px] text-xl rounded-full mt-7 text-white md:hover:text-orange-600 transition-colors duration-200 flex justify-center items-center shadow-md bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-800">
+                    Iniciar Sesión</button>
+                <Image
+                    className="w-[488px] h-[375px] mb-5"
+                    src="/IconLogo.png"
+                    alt="symbol"
+                    width={488}
+                    height={375}
+                />
+            </main >
         </>
     );
 }
