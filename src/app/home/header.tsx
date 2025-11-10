5fef31dd607e934bb36b94cbd2c42445e0406d2c
Marianvsf <marianvsf@gmail.com>
2025-11-09 22:07:01 -0400

    home

--- src/app/home/header.tsx
+++ src/app/home/header.tsx
@@ -12,7 +12,7 @@ export const Header = () => {
                 playsInline
                 preload="auto"
             ></video>
-            <h1 className="m-auto mt-5 text-[54px] font-extrabold text-center">¡Bienvenido a Project Pulse! <small className="text-gray-500 dark:text-gray-400">
+            <h1 className="m-auto mt-5 text-[44px] font-extrabold text-center">¡Bienvenido a Project Pulse! <br /><small className="text-[34px] text-gray-500 dark:text-gray-400">
                 Sigue el pulso de tu proyecto, siente el progreso.
             </small>
             </h1>
