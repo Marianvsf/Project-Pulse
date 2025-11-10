53ba9e47e01a9b98b380241e965e7a673e7691ee
Marianvsf <marianvsf@gmail.com>
2025-11-09 21:50:25 -0400

    footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,13 @@
+export const Footer = () => { 
+return (
+<footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center text-white p-4 bg-gray-900 font-mono">
+  <a
+    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+    href="https://github.com/Marianvsf"
+    target="_blank"
+    rel="noopener noreferrer"
+  >Made by →  Marian Suárez
+  </a>
+</footer>
+)
+}
