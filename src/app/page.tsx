117d5659598d8fa972b9e5fafceddd89723be11e
Marianvsf <marianvsf@gmail.com>
2025-11-09 21:45:59 -0400

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import HomePage from "./home/page";
 
 export default function Home() {
@@ -8,15 +7,6 @@ export default function Home() {
     <>
       <div>
         <HomePage />
-        <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center mb-4">
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://github.com/Marianvsf"
-            target="_blank"
-            rel="noopener noreferrer"
-          >Made by →  Marian Suárez
-          </a>
-        </footer>
       </div>
     </>
   );
