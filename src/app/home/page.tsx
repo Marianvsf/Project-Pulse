ffad1189e15a7347f6b1c014149950ae4c931d70
Marianvsf <marianvsf@gmail.com>
2025-11-14 21:39:05 -0400

    home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -33,6 +33,9 @@ export default function HomePage() {
     const handleNext = (): void => {
         setCurrentSlide((prev) => (prev === images.length - 1 ? 0 : prev + 1));
     };
+    const goToRegistration = () => {
+        router.push("/register");
+    }
 
     return (
         <>
@@ -98,15 +101,20 @@ export default function HomePage() {
             </header>
             <Header />*/}
             <main className="flex flex-col items-center text-center justify-center m-auto max-w-[1010px]">
-                <h1 className="m-auto mt-15 text-[50px] font-extrabold text-center tracking-tight">La gestión de tus proyectos, simplificada y centralizada.</h1>
+                <h1 className="m-auto mt-15 text-[45px] font-extrabold text-center tracking-tight">La gestión de tus proyectos, simplificada y centralizada.</h1>
                 <div className="max-w-[750px]">
                     <small className="text-center text-[25px] text-gray-500 dark:text-gray-400">
                         Project-Pulse ayuda a los equipos organizar tareas, seguir el progreso y colaborar eficazmente, todo en un solo lugar.
                     </small>
                 </div>
-                <button onClick={goToLogin}
-                    className="h-[54px] w-[247px] text-xl rounded-full mt-7 text-white md:hover:text-orange-600 transition-colors duration-200 flex justify-center items-center shadow-md bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-800">
-                    Iniciar Sesión</button>
+                <div className="flex items-center text-center justify-center m-auto space-x-3">
+                    <button onClick={goToRegistration}
+                        className="h-[54px] w-[247px] text-xl rounded-full mt-7 text-white md:hover:text-orange-600 transition-colors duration-200 justify-center items-center shadow-md bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-800">
+                        Registrarse Gratis</button>
+                    <button onClick={goToLogin}
+                        className="h-[54px] w-[247px] text-xl rounded-full mt-7 text-white md:hover:text-orange-600 transition-colors duration-200 justify-center items-center shadow-md bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-800">
+                        Iniciar Sesión</button>
+                </div>
                 <Image
                     className="w-[380px] h-[375px] mb-5"
                     src="/IconLogo.png"
