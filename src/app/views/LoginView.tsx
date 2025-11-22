8d76b54f8f9045040e2d04414ca0a9e9d1a0b8fb
Marianvsf <marianvsf@gmail.com>
2025-11-21 20:37:55 -0400

    nav

--- src/app/views/LoginView.tsx
+++ src/app/views/LoginView.tsx
@@ -33,71 +33,53 @@ export default function LoginView() {
     };
 
     return (
-        <>
-            <nav className="bg-white border-b border-gray-100 dark:bg-gray-900 dark:border-gray-800">
-                <div className="max-w-[1536px] flex flex-wrap items-center mx-auto p-2">
-                    {/* Botón "Volver a atrás"*/}
-                    <div className="flex items-center space-x-3">
+        <form onSubmit={handleSubmit} className="flex flex-col md:flex-row items-center justify-center min-h-screen p-4 md:space-x-12">
+            <div className="flex-shrink-0">
+            </div>
+            <div className="w-full m-auto max-w-md backdrop-blur-sm p-6 rounded-lg">
+                <Image className="m-auto w-[180px] h-[175px] mb-5" src="/IconLogo.png" alt="symbol" width={188} height={75} />
+                <h1 className="text-3xl font-semibold text-center mb-6">Iniciar Sesión</h1>
+                <div className="">
+                    <div>
+                        <label className="block text-left text-xl font-poppins mb-2">Correo electrónico</label>
+                        <input
+                            type="email"
+                            placeholder="Escribe tu correo electrónico"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="h-[45px] w-[400px] p-2 border rounded-md mb-2"
+                        />
+                    </div>
+                    <div>
+                        <label className="block text-left text-xl font-poppins mb-2">Contraseña</label>
+                        <input
+                            type="password"
+                            placeholder="Escribe tu contraseña"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="h-[45px] w-[400px] p-2 border rounded-md mb-2"
+                        />
+                    </div>
+                    <p className="text-center text-sm">
+                        ¿No tienes cuenta?{" "}
                         <button
-                            onClick={() => router.back()}
-                            className="inline-flex items-center text-gray-400 focus:z-10 focus:ring-2 focus:ring-orange-600 rounded-xl hover:bg-gray-500 dark:hover:bg-gray-500"
-                        ><ChevronLeftIcon className="h-8 w-8 mr-1" aria-hidden="true" />
+                            onClick={goToRegistration}
+                            className="text-secondary-500 hover:underline"
+                        >
+                            Regístrate
                         </button>
-                    </div>
-                    <button onClick={goToHome} className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <Image width={10} height={10} src="/Logo.png" className="w-10 h-10 rounded-full bg-gray-500" alt="Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap text-slate-800 dark:text-white">Project Pulse</span>
+                    </p>
+                    <button type="submit"
+                        className="h-[44px] w-[237px] text-xl rounded-xl mt-4 ml-[85px] text-white md:hover:text-orange-600 transition-colors duration-200 flex justify-center items-center shadow-md bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-800">
+                        Entrar
                     </button>
-                </div>
-            </nav>
-            <form onSubmit={handleSubmit} className="flex flex-col md:flex-row items-center justify-center min-h-screen p-4 md:space-x-12">
-                <div className="flex-shrink-0">
-                </div>
-                <div className="w-full m-auto max-w-md backdrop-blur-sm p-6 rounded-lg">
-                    <Image className="m-auto w-[180px] h-[175px] mb-5" src="/IconLogo.png" alt="symbol" width={188} height={75} />
-                    <h1 className="text-3xl font-semibold text-center mb-6">Iniciar Sesión</h1>
-                    <div className="">
-                        <div>
-                            <label className="block text-left text-xl font-poppins mb-2">Correo electrónico</label>
-                            <input
-                                type="email"
-                                placeholder="Escribe tu correo electrónico"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                className="h-[45px] w-[400px] p-2 border rounded-md mb-2"
-                            />
-                        </div>
-                        <div>
-                            <label className="block text-left text-xl font-poppins mb-2">Contraseña</label>
-                            <input
-                                type="password"
-                                placeholder="Escribe tu contraseña"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                className="h-[45px] w-[400px] p-2 border rounded-md mb-2"
-                            />
-                        </div>
-                        <p className="text-center text-sm">
-                            ¿No tienes cuenta?{" "}
-                            <button
-                                onClick={goToRegistration}
-                                className="text-secondary-500 hover:underline"
-                            >
-                                Regístrate
-                            </button>
+                    {error && (
+                        <p className="text-red-500 mt-4 text-center">
+                            {error}
                         </p>
-                        <button type="submit"
-                            className="h-[44px] w-[237px] text-xl rounded-xl mt-4 ml-[85px] text-white md:hover:text-orange-600 transition-colors duration-200 flex justify-center items-center shadow-md bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-800">
-                            Entrar
-                        </button>
-                        {error && (
-                            <p className="text-red-500 mt-4 text-center">
-                                {error}
-                            </p>
-                        )}
-                    </div>
+                    )}
                 </div>
-            </form >
-        </>
+            </div>
+        </form >
     );
 }
