ae03613b571004189df659944ca387e908285896
Marianvsf <marianvsf@gmail.com>
2025-12-02 23:59:08 -0400

    margin

--- src/app/home/header.tsx
+++ src/app/home/header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
                 width={1920}
                 height={400}
                 src={"/assets/video.mp4"}
-                className="block w-full h-[600px] object-cover rounded-2xl"
+                className="block w-full h-[600px] object-cover rounded-2xl mb-10"
                 loop
                 autoPlay
                 muted
