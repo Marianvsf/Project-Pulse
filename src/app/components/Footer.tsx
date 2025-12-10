010ad290cdc1ca306d0625ff18bfc785e6207d9f
Marianvsf <marianvsf@gmail.com>
2025-12-09 20:40:17 -0400

    footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,28 @@ import Image from "next/image"
 
 export const Footer = () => {
   return (
-    <footer className="mt-auto w-full flex-shrink-0 flex gap-3 flex-wrap items-center justify-center text-white p-4 bg-gray-900 font-mono">
+    <footer className="
+      relative 
+      mt-auto w-full flex-shrink-0 flex gap-3 flex-wrap items-center justify-center 
+      text-blue-950 p-4 font-mono
+      /* --- CONFIGURACIÓN DE LA LÍNEA BRILLANTE --- */
+      before:absolute 
+      before:top-0 
+      before:left-1/2 
+      before:-translate-x-1/2 
+      
+      /* Altura y Ancho */
+      before:h-[2px] 
+      before:w-[40%] 
+      
+      /* EL EFECTO DE BRILLO (Degradado + Sombra) */
+      before:bg-gradient-to-r 
+      before:from-transparent 
+      before:via-blue-950 
+      before:to-transparent
+      
+      before:shadow-[0_0_12px_rgba(23,37,84,0.6)]
+    ">
       <a
         className="flex items-center gap-2 hover:underline hover:underline-offset-4"
         href="https://github.com/Marianvsf"
@@ -10,8 +31,8 @@ export const Footer = () => {
         rel="noopener noreferrer"
       >Made by →
         <Image
-          className="logo w-9 h-9 rounded-full border border-white"
-          src="/github.png"
+          className="logo w-9 h-9 rounded-full border border-black"
+          src="/github_black.png"
           alt="logo"
           width={35}
           height={35}
