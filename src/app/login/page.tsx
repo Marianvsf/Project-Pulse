13da6c2b7841f76e0ca4d1c30517482f4be9717d
Marianvsf <marianvsf@gmail.com>
2025-12-13 23:43:10 -0400

    nav

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 
-import Navbar from "../components/navbar/Navbar";
 import LoginView from "../views/LoginView";
 
 
 export default function LoginPage() {
     return (
-        <div>
-            <Navbar />
-            <LoginView />
-        </div>
+        <LoginView />
     )
 }
