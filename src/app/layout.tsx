d36592bbe4339d58a8bbcae9b4b1eec5ba59b97c
Marianvsf <marianvsf@gmail.com>
2025-12-13 23:35:57 -0400

    font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata, Viewport } from "next";
-import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import { Geist, Geist_Mono, Inter, Montserrat, Outfit, Plus_Jakarta_Sans, Poppins } from "next/font/google";
 import "./styles/globals.css";
 import Providers from "./Providers";
 import { Footer } from "./components/Footer";
@@ -18,6 +18,30 @@ const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "700"],
   variable: "--font-poppins",
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-poppins",
+});
+
+const jakarta = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-plus-jakarta-sans",
+});
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-montserrat",
+});
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-outfit",
 });
 
 export const metadata: Metadata = {
@@ -43,7 +67,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased min-h-screen flex flex-col`}
+        className={`${outfit.variable} antialiased min-h-screen flex flex-col`}
       >
         <Providers>
           <main className="flex-1">{children}</main>
