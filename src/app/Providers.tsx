d4c86ca06e229bbd6121d7243ed6c9166118e692
Marianvsf <marianvsf@gmail.com>
2026-04-05 17:16:53 -0400

    session time out

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -2,13 +2,19 @@
 
 import { SessionProvider } from 'next-auth/react';
 import { ReactNode } from 'react';
+import SessionTimeoutManager from './components/SessionTimeoutManager';
 
 interface ProvidersProps {
     children: ReactNode;
 }
 
 const Providers = ({ children }: ProvidersProps) => {
-    return <SessionProvider>{children}</SessionProvider>;
+    return (
+        <SessionProvider>
+            <SessionTimeoutManager />
+            {children}
+        </SessionProvider>
+    );
 };
 
 export default Providers;
